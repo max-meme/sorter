142c2bce707b7f04389cf67152cd74a799fe43fe
max-meme <ziegenfredmax@gmail.com>
2021-06-29 07:41:30 +0200

    fix to test

--- test files/button_test.py
+++ test files/button_test.py
@@ -2,14 +2,15 @@ from time import sleep
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(19, GPIO.OUT)
-GPIO.setup(20, GPIO.OUT)
-GPIO.setup(21, GPIO.OUT)
-GPIO.setup(12, GPIO.OUT)
+GPIO.setup(19, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
     print(GPIO.input(19))
     print(GPIO.input(20))
     print(GPIO.input(21))
     print(GPIO.input(12))
+    print("----")
     sleep(1)
