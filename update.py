b494dd054d9ab5214da48d6466d104a0537af7eb
max-meme <ziegenfredmax@gmail.com>
2021-07-28 12:55:00 +0200

    fixed update

--- update.py
+++ update.py
@@ -1,13 +1,9 @@
 import os
 
 print("removing dir")
-os.system("rm -r ./sorter")
-print("making dir")
-os.system("mkdir sorter")
-print("changing dir")
-os.chdir("./sorter")
+os.system("rm -r ./sorter-main")
 print("downloading from github")
 os.system("wget https://github.com/max-meme/sorter/archive/refs/heads/main.zip")
 print("unzipping")
-os.system("unzip sorter-main.zip")
+os.system("unzip main.zip")
 print("done")
