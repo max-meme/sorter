573be328747aa1ccd341aec84b6a6d8e2852f080
max-meme <ziegenfredmax@gmail.com>
2021-08-01 17:30:58 +0200

    tester added

---
+++ tester.py
@@ -0,0 +1,11 @@
+import os
+print("removing dir")
+os.system("rm -r ./sorter-main")
+print("downloading from github")
+os.system("wget https://github.com/max-meme/sorter/archive/refs/heads/main.zip")
+print("unzipping")
+os.system("unzip main.zip")
+print("removing zip")
+os.system("rm main.zip")
+print("update done")
+os.system("python ./sorter-main/sorter.py")
