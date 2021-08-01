ea4c32ec1f65ffacfb642ba4c163ba22a717cb3c
max-meme <ziegenfredmax@gmail.com>
2021-08-01 18:27:07 +0200

    a

--- update.py
+++ update.py
@@ -1,13 +1,11 @@
 import os
-def update():
-    print("removing dir")
-    os.system("rm -r ./sorter-main")
-    print("downloading from github")
-    os.system("wget https://github.com/max-meme/sorter/archive/refs/heads/main.zip")
-    print("unzipping")
-    os.system("unzip main.zip")
-    print("removing zip")
-    os.system("rm main.zip")
-    print("done")
 
-update()
+print("removing dir")
+os.system("rm -r ./sorter-main")
+print("downloading from github")
+os.system("wget https://github.com/max-meme/sorter/archive/refs/heads/main.zip")
+print("unzipping")
+os.system("unzip main.zip")
+print("removing zip")
+os.system("rm main.zip")
+print("done")
