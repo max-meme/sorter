d72693542aebe21ec1d9addd3c051bf19eb9bf75
max-meme <ziegenfredmax@gmail.com>
2021-08-02 20:18:18 +0200

    arrow debigging yayyy

--- handler_class.py
+++ handler_class.py
@@ -30,4 +30,5 @@ class Handler:
             this.controller.moveto(int(args[0]), int(args[1]), int(args[2]), this.UI)
 
     def arrows(this, x, y):
+        print("arrow triggered")
         this.controller.moverel(x, y, 0, this.UI)
