3530a0a52794bf09e3dc13a4542bed9680f35a15
max-meme <ziegenfredmax@gmail.com>
2021-10-08 03:29:04 +0200

    ard temp

--- test files/I2C_test.py
+++ test files/I2C_test.py
@@ -1,12 +1,33 @@
-from smbus import SMBus;
-from time import sleep;
+import smbus
+import time
+# for RPI version 1, use bus = smbus.SMBus(0)
+bus = smbus.SMBus(1)
 
-addr = 0x8
-bus = SMBus(1)
+# This is the address we setup in the Arduino Program
+address = 0x8
+
+#http://www.raspberry-projects.com/pi/programming-in-python/i2c-programming-in-python/using-the-i2c-interface-2
+def writeData(value):
+    byteValue = StringToBytes(value)    
+    bus.write_i2c_block_data(address,0x00,byteValue) #first byte is 0=command byte.. just is.
+    return -1
+
+
+def StringToBytes(val):
+        retVal = []
+        for c in val:
+                retVal.append(ord(c))
+        return retVal
 
 while True:
-    sleep(1)
-    bus.write_byte(addr, 0x0)
-    sleep(1)
-    bus.write_byte(addr, 0x1)
+    print("sending")
+    writeData("test")
+    time.sleep(5)
 
+    print('OPEN')
+    writeData("OPEN-00-00")
+    time.sleep(7)
+
+    print('WIN')
+    writeData("WIN-12-200")
+    time.sleep(7)
