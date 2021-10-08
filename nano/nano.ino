2a19a6cc71ecf856861b50acdcb3200b0ac6043b
max-meme <ziegenfredmax@gmail.com>
2021-10-08 02:40:44 +0200

    testing more i2c

--- nano/nano.ino
+++ nano/nano.ino
@@ -3,6 +3,9 @@
 //---Globals---
 
 const int DRV_count = 6;
+const int adress = 0x8;
+char temp[32];
+volatile boolean receiveFlag = false;
 
 //All pins used
 const int pins[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 20, 21, LED_BUILTIN};
@@ -12,24 +15,40 @@ const int DRVs[DRV_count][2] = {{2, 3}, {4, 5}, {6, 7}, {8, 9}, {10, 11}, {14, 1
 
 void setup() {
   //set all pins as out
-  for(int i = 0; i < 18; i++)
+  for(int i = 0; i < 19; i++)
   {
     pinMode(pins[i], OUTPUT);
   }
   //setup I2C between Pi and Nano
-  Wire.begin(0x8);
+  Wire.begin(adress);
   Wire.onReceive(recieveEvent);
 }
 
 void recieveEvent(int howMany) {
+  int m = 0;
   while(Wire.available()) {
-    char m = Wire.read();
+    m = Wire.read();
   }
+  
+  for (int i = 0; i < howMany; i++) {
+      temp[i] = Wire.read();
+      temp[i + 1] = '\0'; //add null after ea. char
+    }
+
+  //RPi first byte is cmd byte so shift everything to the left 1 pos so temp contains our string
+  for (int i = 0; i < howMany; ++i)
+    temp[i] = temp[i + 1];
   blinkLED();
+  receiveFlag = true;
 }
 
 void loop() {
   //testDRVs();
+  delay(100);
+  if (receiveFlag == true) {
+    Serial.println(temp);
+    receiveFlag = false;
+  }
 }
 
 void testDRVs() {
@@ -57,7 +76,7 @@ void testDRVs() {
 
 void blinkLED() {
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(1);
+  delay(100);
   digitalWrite(LED_BUILTIN, LOW);
-  delay(1);
+  delay(100);
 }
